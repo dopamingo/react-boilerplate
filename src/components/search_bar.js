abd84774631f981323f00ce8c4bf5220ea9e6ecd
dopaming <over_ray@naver.com>
2021-07-13 02:24:21 +0900

    class component upgrade

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,6 @@
 // JSX를 JS로 변환할때마다 React.createElement와 같은 형태로 변환됨
 // 따라서 JSX를 쓰는 모든 컴포넌트에 임포트 해야함
-import React from 'react';
+import React, { Component } from 'react';
 
 // 함수형 컴포넌트
 // const SearchBar = () => {
