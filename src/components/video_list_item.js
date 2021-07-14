2f2ac2d2b4f2afae4bda355a5bac6c5235b0906c
dopaming <over_ray@naver.com>
2021-07-14 23:59:59 +0900

    selectVideo

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
 	// const video = props.video; ES6 ({video}) 와 같음
 	// 요소의 첫번째 객체가 비디오 프로퍼티로 나타나게 되고,
 	// 비디오를 가져와 새로운 video 나 새로운 변수를 생성하라는 의미
 	const imageUrl = video.snippet.thumbnails.default.url;
 	return (
-		<li className="list-group-item">
+		<li onClick={() => onVideoSelect(video)} className="list-group-item">
 			<div className="video-list media">
 				<div className="media-left">
 					<img className="media-object" src={imageUrl} />
--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom'; // React DOM 실제로 DOM에 렌더링하는 
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'; // 설치된 라이브러리가 아니라면 참조경로를 제공해야함
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDYwzrLhbcsfs6oo7WxLkMrnRMod3dc0Ik';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] }; // 스테이트에 비디오를 빈 배열로 세팅
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    }; // 스테이트에 비디오를 빈 배열로 세팅
     
     // 컴포넌트가 렌더링 되면서 검색을 실행하고, 검색이 완료되면 비디오에 값들을 업데이트
-    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-      this.setState({ videos: data }); 
+    YTSearch({key: API_KEY, term: 'Surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      }); 
     });
   }
 
@@ -21,7 +28,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+        // 비디오를 가져와서 선택한 비디오를 업데이트 그리고 이 프로퍼티를 비디오 리스트로 전달한다
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
       </div>
     );
   }
